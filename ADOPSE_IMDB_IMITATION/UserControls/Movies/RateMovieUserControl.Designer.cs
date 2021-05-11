01d8cee7d4e5a02ca6981c78519217e2d56167a6
Nena Michailidou <nenamichailidou@gmail.com>
2021-05-11 22:53:48 +0300

    adding the remaining 5 rating btns till 10

--- ADOPSE_IMDB_IMITATION/UserControls/Movies/RateMovieUserControl.Designer.cs
+++ ADOPSE_IMDB_IMITATION/UserControls/Movies/RateMovieUserControl.Designer.cs
@@ -36,6 +36,11 @@ namespace ADOPSE_IMDB_IMITATION.UserControls.Movies
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.SaveRatingBtn = new System.Windows.Forms.Button();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
             this.RateMovieRadioButtonGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,6 +57,11 @@ namespace ADOPSE_IMDB_IMITATION.UserControls.Movies
             // 
             // RateMovieRadioButtonGroupBox
             // 
+            this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton10);
+            this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton9);
+            this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton8);
+            this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton7);
+            this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton6);
             this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton5);
             this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton4);
             this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton3);
@@ -59,7 +69,7 @@ namespace ADOPSE_IMDB_IMITATION.UserControls.Movies
             this.RateMovieRadioButtonGroupBox.Controls.Add(this.radioButton1);
             this.RateMovieRadioButtonGroupBox.Location = new System.Drawing.Point(3, 3);
             this.RateMovieRadioButtonGroupBox.Name = "RateMovieRadioButtonGroupBox";
-            this.RateMovieRadioButtonGroupBox.Size = new System.Drawing.Size(191, 38);
+            this.RateMovieRadioButtonGroupBox.Size = new System.Drawing.Size(378, 38);
             this.RateMovieRadioButtonGroupBox.TabIndex = 1;
             this.RateMovieRadioButtonGroupBox.TabStop = false;
             this.RateMovieRadioButtonGroupBox.Text = "Rate the movie";
@@ -110,7 +120,7 @@ namespace ADOPSE_IMDB_IMITATION.UserControls.Movies
             // 
             // SaveRatingBtn
             // 
-            this.SaveRatingBtn.Location = new System.Drawing.Point(200, 14);
+            this.SaveRatingBtn.Location = new System.Drawing.Point(387, 17);
             this.SaveRatingBtn.Name = "SaveRatingBtn";
             this.SaveRatingBtn.Size = new System.Drawing.Size(40, 23);
             this.SaveRatingBtn.TabIndex = 3;
@@ -118,12 +128,67 @@ namespace ADOPSE_IMDB_IMITATION.UserControls.Movies
             this.SaveRatingBtn.UseVisualStyleBackColor = true;
             this.SaveRatingBtn.Click += new System.EventHandler(this.SaveRatingBtn_Click);
             // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(191, 17);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(31, 17);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(228, 17);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(31, 17);
+            this.radioButton7.TabIndex = 6;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(265, 17);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(31, 17);
+            this.radioButton8.TabIndex = 7;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "8";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(302, 17);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(31, 17);
+            this.radioButton9.TabIndex = 8;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "9";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(339, 17);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(37, 17);
+            this.radioButton10.TabIndex = 9;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "10";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
             // RateMovieUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(249, 49);
+            this.ClientSize = new System.Drawing.Size(434, 49);
             this.Controls.Add(this.SaveRatingBtn);
             this.Controls.Add(this.RateMovieRadioButtonGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -144,5 +209,10 @@ namespace ADOPSE_IMDB_IMITATION.UserControls.Movies
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Button SaveRatingBtn;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
     }
 }
