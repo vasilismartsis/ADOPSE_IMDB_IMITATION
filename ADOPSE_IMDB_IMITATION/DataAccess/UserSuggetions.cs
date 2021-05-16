1931b9dd578f18c4978589293b47a13f553e25d9
MarTsou98 <73964623+MarTsou98@users.noreply.github.com>
2021-05-16 03:46:58 +0300

    PopUpVerification && UserSuggetion(NotDone)

---
+++ ADOPSE_IMDB_IMITATION/DataAccess/UserSuggetions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOPSE_IMDB_IMITATION.DataAccess
+{
+    class UserSuggetions
+    {
+    }
+}
